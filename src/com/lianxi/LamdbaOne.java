cb12e3a91c51c46e5505a5a45fc6c6245e43a6b5
柯鑫 <2337594954@qq.com>
2019-05-01 12:20:50 +0800

    解释说明Lamdba文件

--- src/com/lianxi/LamdbaOne.java
+++ src/com/lianxi/LamdbaOne.java
@@ -5,6 +5,7 @@ public class LamdbaOne {
 
     public static void main(String[] args) {
         System.out.println("...................");
+        System.out.println("hahah ");
     }
 
    /* 基础题
